9315281415bdc13dc29cd3c745a7b68a3b911cc5
leesahanders <lisa.anders@rstudio.com>
2022-12-08 13:36:09 -0600

    syncing changes

--- test/app.R
+++ test/app.R
@@ -40,9 +40,13 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$distPlot <- renderPlot({
+    
+    
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    Sys.sleep(2)
     
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
