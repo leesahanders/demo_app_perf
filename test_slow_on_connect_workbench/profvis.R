3c5b09b4cf237b556982d7ca582bd4b43237ada7
leesahanders <lisa.anders@rstudio.com>
2023-02-22 15:38:57 +0000

    Syncing changes

--- test_slow_on_connect_workbench/profvis.R
+++ test_slow_on_connect_workbench/profvis.R
@@ -8,6 +8,48 @@ library(deSolve)
 # Shiny app
 profvis({runApp()})
 
+# Simple example
+profvis({
+  data(diamonds, package = "ggplot2")
+  
+  plot(price ~ carat, data = diamonds)
+  m <- lm(price ~ carat, data = diamonds)
+  abline(m, col = "red")
+})
 
+# More complex example
+profvis({
+  # generate a dataset
+  data(diamonds, package = "ggplot2")
+  
+  # save it 
+  write.csv(diamonds, "diamonds.csv")
+  
+  # load it
+  csv_diamonds <- read.csv("diamonds.csv")
+  
+  # summarize
+  summary(diamonds)
+  
+  # plot it  
+  plot(price ~ carat, data = csv_diamonds)
+  m <- lm(price ~ carat, data = csv_diamonds)
+  abline(m, col = "red")
+  
+  #create histogram of values for price
+  ggplot(data=csv_diamonds, aes(x=price)) +
+    geom_histogram(fill="steelblue", color="black") +
+    ggtitle("Histogram of Price Values")
+  
+  #create scatterplot of carat vs. price, using cut as color variable
+  ggplot(data=diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point()
+  
+  #create scatterplot of carat vs. price, using cut as color variable
+  ggplot(data=diamonds, aes(x=carat, y=price, color=cut)) + 
+    geom_point()
+  
+  # Examples from: https://www.statology.org/diamonds-dataset-r/#:~:text=The%20diamonds%20dataset%20is%20a,the%20diamonds%20dataset%20in%20R. 
+})
 
 
